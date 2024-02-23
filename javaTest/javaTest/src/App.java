7a6c44e8e2f35bc8e59f92352a07b1d54254a59e
M <dkmoon0530@gmail.com>
2024-02-23 18:12:13 +0900

    create project

---
+++ javaTest/javaTest/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 10;
+        int b = 5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
