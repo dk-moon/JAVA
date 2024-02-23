913bb6f1b5ab0acc688a075f7f90e7fc2f6ab02a
M <dkmoon0530@gmail.com>
2024-02-23 19:47:16 +0900

    solve add

---
+++ Chapter03/Chapter03/src/Solve01.java
@@ -0,0 +1,45 @@
+/*
+ * 1번 문제
+ *  Write a program that reads an integer between 0 and 1000
+ * and multiplies all the digits in the integer.
+ *  0부터 1000까지 중 임의의 수를 입력 받아, 각 자리 숫자를 곱하는 프로그램 작성
+ *  For example, if an integer is 932, the multiplication of all
+ * its digit is 9 * 3 * 2 = 54.
+ * 
+ *  Here is a sample run:
+ * Enter an integer between 0 and 1000: 521
+ * The multiplication of all difits in 521 is 10
+ */
+
+ import java.util.Scanner;
+
+public class Solve01 {
+    public static void main(String[] args) throws Exception {
+        // scanner 선언
+        Scanner input = new Scanner(System.in);
+        
+        // 사용자에게 0부터 1000 사이의 정수를 입력
+        System.out.print("Enter an integer between 0 and 1000: ");
+        int input_num = input.nextInt();
+        
+        // 입력받은 수가 조건을 만족하는지 확인
+        int number = input_num;
+        if (number < 0 || number > 1000) {
+            System.out.println("The number must be between 0 and 1000.");
+            return; // 프로그램을 종료
+        }
+
+        // 각 자리수를 곱할 결과를 저장할 변수. 초기값은 1로 설정
+        int product = 1;
+
+        // number가 0이 아닐 때까지 각 자리의 숫자 곱셈
+        while (number > 0) {
+            product *= number % 10; // 현재 숫자의 맨 오른쪽 자리를 곱
+            number /= 10; // 다음 자리수
+        }
+        
+        // 결과를 출력
+        System.out.println(String.format("The multiplication of all digits in %d is %d", input_num, product));
+        input.close();
+    }
+}
---
+++ Chapter03/Chapter03/src/Solve02.java
@@ -0,0 +1,44 @@
+/*
+ * 2번 문제
+ *  Write a program that reads three edges for a triangle and
+ * computes the perimeter if the input is valid.
+ *  Otherwise, display that the input is invalid.
+ *  The input is valid for triangle edges if the sum of every
+ * pair of two edges is greater than the remaining edge.
+ *  삼각형의 3개 변의 길이(실수형)를 사용자로부터 입력받아, 유효하면
+ * 삼각형의 둘레를 계산하는 프로그램을 작성하시오. 단, 삼각형의 3변은 모든
+ * 조합에 대해 2개 변의 합이 다른 한 변보다 길어야 유효하다.
+ * 
+ * Here is a sample run:
+ * Enter three edges (length in double) : 2.0 3.0 5.0
+ * Input is invalid
+ * Enter three deges (length in double) : 3.0 3.0 4.0
+ * The perimeter is 10.0
+ */
+
+ import java.util.Scanner;
+
+public class Solve02 {
+    public static void main(String[] args) throws Exception {
+        // scanner 선언
+        Scanner input = new Scanner(System.in);
+        
+        // 사용자로부터 3개의 변 입력
+        System.out.print("Enter three edges (length in double): ");
+        double edge1 = input.nextDouble();
+        double edge2 = input.nextDouble();
+        double edge3 = input.nextDouble();
+
+        // 2변의 합과 다른 한 변의 길이 확인
+        if (edge1 + edge2 > edge3 && edge1 + edge3 > edge2 && edge2 + edge3 > edge1) {
+            // 둘레 계산
+            double perimeter = edge1 + edge2 + edge3;
+            System.out.println("The perimeter is " + perimeter);
+        } else {
+            // 조건 미달성
+            System.out.println("Input is invalid");
+        }
+
+        input.close();
+    }
+}
